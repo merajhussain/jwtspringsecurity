d7cc4a0d5bfb53d7b75f47a1273dd75b94a4322f
Meraj Syed <merajhussain1@gmail.com>
2024-07-13 19:50:57 +0530

    Spring security with jwt authentication

---
+++ src/main/java/com/test/security/springsec/security/JwtResponse.java
@@ -0,0 +1,14 @@
+package com.test.security.springsec.security;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+public class JwtResponse {
+    private String jwtToken;
+    private String username;
+}
